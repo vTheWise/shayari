1dba134f9f285812dae5077e31e12d3544f450f3
VTheWise <shrivastava_vishal@outlook.com>
2025-04-22 09:33:06 -0500

    Update GitHub username and repository links

--- publishconf.py
+++ publishconf.py
@@ -8,8 +8,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-# Replace 'your-github-username' with your actual GitHub username
-SITEURL = 'https://your-github-username.github.io/shayari'
+# GitHub Pages URL
+SITEURL = 'https://vthewise.github.io/shayari'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
